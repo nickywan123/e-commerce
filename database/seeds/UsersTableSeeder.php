2fc16412f708a52adabdddb54aca245bcdfdf074
Wan Shahruddin <wanshahruddin95@gmail.com>
2020-02-26 19:45:53 +0800

    Fixed UserTableSeeders

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -14,19 +14,16 @@ class UsersTableSeeder extends Seeder
     public function run()
     {
         User::create([
-            'name' => 'Store 1',
             'email' => 'store1@email.com',
             'password' => bcrypt('password'),
         ]);
 
         User::create([
-            'name' => 'Store 2',
             'email' => 'store2@email.com',
             'password' => bcrypt('password'),
         ]);
 
         User::create([
-            'name' => 'Store 3',
             'email' => 'store3@email.com',
             'password' => bcrypt('password'),
         ]);
