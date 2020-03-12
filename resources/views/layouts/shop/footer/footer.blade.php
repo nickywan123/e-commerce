63e33514f6c830f0ac435b2e2601f2b5dd2dfb30
wanshahruddin95 <wanshahruddin95@gmail.com>
2020-03-12 10:59:32 +0800

    Fix navigation & shop page.

---
+++ resources/views/layouts/shop/footer/footer.blade.php
@@ -0,0 +1,35 @@
+<footer class="site-footer">
+    <div class="row">
+
+        <div id="foot1" class="col-md-2 col-sm-9 ">
+
+            <h6 style="color:#fbcc34">Information</h6>
+            <ul class="footer-links">
+                <li><a href="http://scanfcode.com/category/c-language/">About Us</a></li>
+                <li><a href="http://scanfcode.com/category/front-end-development/">FAQ</a></li>
+                <li><a href="http://scanfcode.com/category/back-end-development/">Warranty</a></li>
+                <li><a href="http://scanfcode.com/category/java-programming-language/">Partnership</a></li>
+                <li><a href="http://scanfcode.com/category/android/">Privacy Policy</a></li>
+                <li><a href="http://scanfcode.com/category/templates/">Contact Us</a></li>
+            </ul>
+        </div>
+
+        <div id="foot2" class="col-md-3 col-sm-3">
+            <h6>My Account</h6>
+            <ul class="footer-links">
+                <li><a href="http://scanfcode.com/category/c-language/">Sign In</a></li>
+                <li><a href="http://scanfcode.com/category/front-end-development/">View Cart</a></li>
+                <li><a href="http://scanfcode.com/category/back-end-development/">My Wishlist</a></li>
+
+            </ul>
+        </div>
+
+
+
+    </div>
+</footer>
+</div>
+
+<section class="container-fluid " id="foot5">
+    <h6 class="text-right" style="color:white;">@ 2020 Bujishu. All Rights Reserved</h6>
+</section>
---
+++ resources/views/layouts/shop/navigation/navigation.blade.php
@@ -0,0 +1,99 @@
+<div class="middleBar">
+    <div class="container-90">
+        <div class="row d-flex">
+            <div class="col-sm-2 vertical-align hidden-xs">
+                <div class="row">
+                    <div class="col-6 my-auto text-left p-1">
+                        <a href="javascript:void(0);">
+                            <i class="fa fa-bars navigation-icon leftmenutrigger "></i>
+                        </a>
+
+                    </div>
+                    <div class="col-6 text-right my-auto">
+                        <a href="javascript:void(0);">
+                            <img class="navigation-logo" style="margin-right: 30px;" src="http://demo3.bujishu.com/storage/logo/bujishu.png" alt="">
+                        </a>
+                    </div>
+                </div>
+
+            </div>
+            <!-- end col -->
+            <div class="col-sm-6 vertical-align text-center my-auto">
+                <form>
+                    <div class="row grid-space-1">
+                        <div class="col-12 my-auto">
+                            <select class="form-control input-lg w-25 hidden-sm" style="border-right:5px solid #fbcc34;margin-right:-5px; border-top-left-radius: 10px;border-bottom-left-radius: 10px;" name="category">
+                                <option value="all">All Categories</option>
+                                <optgroup label="Mens">
+                                    <option value="shirts">Shirts</option>
+                                    <option value="coats-jackets">Coats & Jackets</option>
+                                    <option value="underwear">Underwear</option>
+                                    <option value="sunglasses">Sunglasses</option>
+                                    <option value="socks">Socks</option>
+                                    <option value="belts">Belts</option>
+                                </optgroup>
+                            </select>
+                            <input type="text" name="keyword" class="form-control input-lg w-65-md w-85-sm" style="border-right:5px solid #fbcc34;margin-right:-5px;" placeholder="Search">
+                            <button class="btn" style="background-color:#ffffff; color:black ;border-top-right-radius: 10px; border-bottom-right-radius:10px;"><i class="fa fa-search"></i></button>
+                        </div>
+                        <!-- <div class="col-6">
+                                <input type="text" name="keyword" class="form-control input-lg" placeholder="Search">
+                            </div>
+                            <div class="col-3 text-left">
+                                <button class="btn btn-primary"><i class="fa fa-search"></i></button> 
+                            </div> -->
+                        <!-- end col -->
+                    </div>
+                    <!-- end row -->
+                </form>
+            </div>
+            <!-- end col -->
+            <div class="col-sm-4 vertical-align hidden-xs my-auto">
+                <!-- <div class="header-item mr-5">
+                        <a href="javascript:void(0);" data-toggle="tooltip" data-placement="top" title=""
+                            data-original-title="Wishlist"> <i class="fa fa-heart-o"></i> <sub>32</sub> </a>
+                    </div>
+                    <div class="header-item">
+                        <a href="javascript:void(0);" data-toggle="tooltip" data-placement="top" title=""
+                            data-original-title="Compare"> <i class="fa fa-refresh"></i> <sub>2</sub> </a>
+                    </div> -->
+                <ul class="nav justify-content-center-sm float-md-right my-auto hidden-sm">
+                    <li class="nav-item m-1">
+                        <a class="nav-link colorlink-gold" href="">My Account</a>
+                    </li>
+                    <li class="nav-item m-1">
+                        <a class="nav-link colorlink-gold" href="">Login</a>
+                    </li>
+                    <li class="nav-item m-1">
+                        <a class="nav-link colorlink-gold" href="">Register</a>
+                    </li>
+                    <li class="nav-item m-1">
+                        <a href="" class=" btn grad1"><b>PANEL</b></a>
+                    </li>
+                    <li class="nav-item m-1">
+                        <a href="" class="btn grad1"><b>DEALER</b></a>
+                    </li>
+                </ul>
+            </div>
+            <!-- end col -->
+        </div>
+        <!-- end  row -->
+    </div>
+</div>
+
+<div class="bottom-bar shadow-sm" style="border-bottom:#ffca05 2px solid">
+    <div class="container-90">
+        <div class="row display-table">
+            <div class="col-sm-12 justify-content-end-md justify-content-center-sm ">
+                <ul class="nav justify-content-center-sm float-right-md mb-0 ">
+                    <li class="nav-item m-1">
+                        <a class="nav-link colorlink-gold" href=""><i class="fa fa-heart-o font-15 mr-1"></i> My Wishlist</a>
+                    </li>
+                    <li class="nav-item m-1">
+                        <a class="nav-link  colorlink-gold" href=""> <img class=”img-logo” src="{{ asset('images/cart.png') }}" style="width:28px; height: 30px;" /> My Cart</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</div>
