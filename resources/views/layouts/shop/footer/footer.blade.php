b0fd1c568dda1bdb2dd7dfca2a8fb19920734e82
wanshahruddin95 <wanshahruddin95@gmail.com>
2020-03-15 22:43:56 +0800

    Adjust footer and sidebar.

--- resources/views/layouts/shop/footer/footer.blade.php
+++ resources/views/layouts/shop/footer/footer.blade.php
@@ -1,4 +1,4 @@
-<footer style="max-width:100%; overflow: hidden; border-top: 3px solid #fbcc34; padding-top: 0.5rem;">
+<footer style="max-width:100%; overflow: hidden; border-top: 3px solid #fbcc34; padding-top: 0.5rem; background-color: #000;">
     <div class="row pl-2 pr-2">
         <div class="col-6 col-md-2 text-center text-md-left">
             <h5 class="pr-1 pl-1 bujishu-gold">Information</h5>
@@ -40,6 +40,6 @@
     </div>
 </footer>
 
-<section class="container-fluid">
+<section class="container-fluid" style="background-color: #000;">
     <h6 class="text-right" style="color:white;">@ 2020 Bujishu. All Rights Reserved</h6>
 </section>
--- resources/views/layouts/shop/navigation/sidebar.blade.php
+++ resources/views/layouts/shop/navigation/sidebar.blade.php
@@ -6,27 +6,27 @@
     <div class="sidebar-header">
         <img class="navigation-logo" style="margin-right: 30px;" src="{{ asset('storage/logo/bujishu.png') }}" alt="">
     </div>
-     
+
     <ul class="list-unstyled components">
-        <h4 ><strong>Shop By Category</strong></h4>
-        <li class="active">
-            <a href="#homeSubmenu">Bedsheet & Mattress</a>
-            <a href="#">Cupboard</a>
-            <a href="#pageSubmenu"  >Tables & Chairs</a>
-            <a href="#">Carpet</a>
-            <a href="#">Tiles</a>
-            <a href="#">Lighting</a>
-            <a href="#">Wall Papers</a>
-            <a href="#">Roof</a>
-            <a href="#">Doors</a>
-            <a href="#">Window</a>
-            <a href="#">Auxillary Propesrity Items</a>
-            <a href="#">Product & Services</a>
-            
+        <h4 style="color: #000; padding: 10px;"><strong>Shop By Category</strong></h4>
+        <li class="active text-dark">
+            <a class="text-dark" href="#homeSubmenu">Bedsheet & Mattress</a>
+            <a class="text-dark" href="#">Cupboard</a>
+            <a class="text-dark" href="#pageSubmenu">Tables & Chairs</a>
+            <a class="text-dark" href="#">Carpet</a>
+            <a class="text-dark" href="#">Tiles</a>
+            <a class="text-dark" href="#">Lighting</a>
+            <a class="text-dark" href="#">Wall Papers</a>
+            <a class="text-dark" href="#">Roof</a>
+            <a class="text-dark" href="#">Doors</a>
+            <a class="text-dark" href="#">Window</a>
+            <a class="text-dark" href="#">Auxillary Propesrity Items</a>
+            <a class="text-dark" href="#">Product & Services</a>
+
         </li>
 
-         <!-- For future use of navigation bar expand -->
-            {{-- <nav id="mysidebarmenu" class="amazonmenu">
+        <!-- For future use of navigation bar expand -->
+        {{-- <nav id="mysidebarmenu" class="amazonmenu">
                 <ul>
                     <li><a href="#">Item 1</a></li>
                     <li><a href="#">Folder 0</a>
