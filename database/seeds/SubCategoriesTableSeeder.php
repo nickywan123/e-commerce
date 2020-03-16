0adfdd3ebbe3c1ea714484f7c945e9a1f8b2c31c
wanshahruddin95 <wanshahruddin95@gmail.com>
2020-03-16 12:27:28 +0800

    Doing top level category page. Started using new category structure.

--- database/seeds/SubCategoriesTableSeeder.php
+++ database/seeds/SubCategoriesTableSeeder.php
@@ -13,108 +13,108 @@ class SubCategoriesTableSeeder extends Seeder
      */
     public function run()
     {
-        SubCategory::create([
-            'name' => 'wiring',
-            'slug' => 'wiring',
-            'parent_category_id' => '1'
-        ])->image()->create([
-            'url' => 'electrical.jpg'
-        ]);
+        // SubCategory::create([
+        //     'name' => 'wiring',
+        //     'slug' => 'wiring',
+        //     'parent_category_id' => '1'
+        // ])->image()->create([
+        //     'url' => 'electrical.jpg'
+        // ]);
 
-        SubCategory::create([
-            'name' => 'lamp',
-            'slug' => 'lamp',
-            'parent_category_id' => '1'
-        ])->image()->create([
-            'url' => 'lamp.jpg'
-        ]);
+        // SubCategory::create([
+        //     'name' => 'lamp',
+        //     'slug' => 'lamp',
+        //     'parent_category_id' => '1'
+        // ])->image()->create([
+        //     'url' => 'lamp.jpg'
+        // ]);
 
-        SubCategory::create([
-            'name' => 'lighting',
-            'slug' => 'lighting',
-            'parent_category_id' => '1'
-        ])->image()->create([
-            'url' => 'lighting.jpg'
-        ]);
+        // SubCategory::create([
+        //     'name' => 'lighting',
+        //     'slug' => 'lighting',
+        //     'parent_category_id' => '1'
+        // ])->image()->create([
+        //     'url' => 'lighting.jpg'
+        // ]);
 
-        SubCategory::create([
-            'name' => 'plaster ceilling',
-            'slug' => 'plaster-ceilling',
-            'parent_category_id' => '2'
-        ])->image()->create([
-            'url' => 'plaster-ceiling.jpg'
-        ]);
+        // SubCategory::create([
+        //     'name' => 'plaster ceilling',
+        //     'slug' => 'plaster-ceilling',
+        //     'parent_category_id' => '2'
+        // ])->image()->create([
+        //     'url' => 'plaster-ceiling.jpg'
+        // ]);
 
-        SubCategory::create([
-            'name' => 'cabinet',
-            'slug' => 'cabinet',
-            'parent_category_id' => '3'
-        ])->image()->create([
-            'url' => 'build-in-cabinets.jpg'
-        ]);
+        // SubCategory::create([
+        //     'name' => 'cabinet',
+        //     'slug' => 'cabinet',
+        //     'parent_category_id' => '3'
+        // ])->image()->create([
+        //     'url' => 'build-in-cabinets.jpg'
+        // ]);
 
-        SubCategory::create([
-            'name' => 'sofa',
-            'slug' => 'sofa',
-            'parent_category_id' => '3'
-        ])->image()->create([
-            'url' => 'sofa.jpg'
-        ]);
+        // SubCategory::create([
+        //     'name' => 'sofa',
+        //     'slug' => 'sofa',
+        //     'parent_category_id' => '3'
+        // ])->image()->create([
+        //     'url' => 'sofa.jpg'
+        // ]);
 
-        SubCategory::create([
-            'name' => 'dining set',
-            'slug' => 'dining-set',
-            'parent_category_id' => '3'
-        ])->image()->create([
-            'url' => 'dining-set.jpg'
-        ]);
+        // SubCategory::create([
+        //     'name' => 'dining set',
+        //     'slug' => 'dining-set',
+        //     'parent_category_id' => '3'
+        // ])->image()->create([
+        //     'url' => 'dining-set.jpg'
+        // ]);
 
-        SubCategory::create([
-            'name' => 'curtain / bedsheet',
-            'slug' => 'curtain-bedsheet',
-            'parent_category_id' => '3'
-        ])->image()->create([
-            'url' => 'curtain.jpg'
-        ]);
+        // SubCategory::create([
+        //     'name' => 'curtain / bedsheet',
+        //     'slug' => 'curtain-bedsheet',
+        //     'parent_category_id' => '3'
+        // ])->image()->create([
+        //     'url' => 'curtain.jpg'
+        // ]);
 
-        SubCategory::create([
-            'name' => 'clock',
-            'slug' => 'clock',
-            'parent_category_id' => '3'
-        ])->image()->create([
-            'url' => 'clock.jpg'
-        ]);
+        // SubCategory::create([
+        //     'name' => 'clock',
+        //     'slug' => 'clock',
+        //     'parent_category_id' => '3'
+        // ])->image()->create([
+        //     'url' => 'clock.jpg'
+        // ]);
 
-        SubCategory::create([
-            'name' => 'wallpaper',
-            'slug' => 'wallpaper',
-            'parent_category_id' => '3'
-        ])->image()->create([
-            'url' => 'wallpaper.jpg'
-        ]);
+        // SubCategory::create([
+        //     'name' => 'wallpaper',
+        //     'slug' => 'wallpaper',
+        //     'parent_category_id' => '3'
+        // ])->image()->create([
+        //     'url' => 'wallpaper.jpg'
+        // ]);
 
-        SubCategory::create([
-            'name' => 'carpet',
-            'slug' => 'carpet',
-            'parent_category_id' => '3'
-        ])->image()->create([
-            'url' => 'carpet.jpg'
-        ]);
+        // SubCategory::create([
+        //     'name' => 'carpet',
+        //     'slug' => 'carpet',
+        //     'parent_category_id' => '3'
+        // ])->image()->create([
+        //     'url' => 'carpet.jpg'
+        // ]);
 
-        SubCategory::create([
-            'name' => 'gate',
-            'slug' => 'gate',
-            'parent_category_id' => '4'
-        ])->image()->create([
-            'url' => 'gate.jpg'
-        ]);
+        // SubCategory::create([
+        //     'name' => 'gate',
+        //     'slug' => 'gate',
+        //     'parent_category_id' => '4'
+        // ])->image()->create([
+        //     'url' => 'gate.jpg'
+        // ]);
 
-        SubCategory::create([
-            'name' => 'paint',
-            'slug' => 'paint',
-            'parent_category_id' => '5'
-        ])->image()->create([
-            'url' => 'paint.jpg'
-        ]);
+        // SubCategory::create([
+        //     'name' => 'paint',
+        //     'slug' => 'paint',
+        //     'parent_category_id' => '5'
+        // ])->image()->create([
+        //     'url' => 'paint.jpg'
+        // ]);
     }
 }
--- resources/views/shop/catalog/top-level.blade.php
+++ resources/views/shop/catalog/top-level.blade.php
@@ -10,10 +10,10 @@
     </div>
 
     <div class="row">
-        <div class="col-2">
+        <div class="col-2 hidden-sm">
             <div class="p-3 shadow-sm" style="background-color: #ffffff;">
                 <h5 class="text-dark">Panel</h5>
-                <ul class="list-unstyled pl-2">
+                <ul class="list-unstyled pl-2 pr-2">
                     <li>
                         <a href="">Company A</a>
                     </li>
@@ -26,7 +26,7 @@
                 </ul>
 
                 <h5 class="text-dark">Price</h5>
-                <ul class="list-unstyled pl-2">
+                <ul class="list-unstyled pl-2 pr-2">
                     <li>
                         <a href="">Under RM25</a>
                     </li>
@@ -42,19 +42,118 @@
                     <li>
                         <a href="">RM200 & Above</a>
                     </li>
-                    <li>
-                        <div class="form-row">
-                            <div class="form-group col-6">
-                                <input type="text" class="form-control" id="price_min" placeholder="Min">
+                    <li class="p-1">
+                        <form action="" class="form-inline">
+                            <div class="form-inline row">
+                                <input type="text" class="form-control col-4 border-rounded-0" id="price_min" placeholder="Min">
+                                <input type="text" class="form-control col-4 border-rounded-0" id="price_max" placeholder="Max">
+                                <button class="btn border-rounded-0 bg-bujishu-gold"><i class="fa fa-play"></i></button>
                             </div>
-                            <div class="form-group col-6">
-                                <input type="text" class="form-control" id="price_max" placeholder="Max">
-                            </div>
-                        </div>
+                        </form>
                     </li>
                 </ul>
+
+                <h5 class="text-dark">Color</h5>
+                <ul class="list-unstyled pl-2 pr-2">
+                    <li>
+                        <a href="">White</a>
+                    </li>
+                    <li>
+                        <a href="">Beige</a>
+                    </li>
+                    <li>
+                        <a href="">Red</a>
+                    </li>
+                    <li>
+                        <a href="">Maroon</a>
+                    </li>
+                    <li>
+                        <a href="">Grey</a>
+                    </li>
+                    <li>
+                        <a href="">Black</a>
+                    </li>
+                </ul>
+
+                <h5 class="text-dark">Rating</h5>
+                <ul class="list-unstyled pl-2 pr-2">
+                    <li>
+                        <a href="">
+                            <span class="fa fa-star checked"></span>
+                            <span class="fa fa-star checked"></span>
+                            <span class="fa fa-star checked"></span>
+                            <span class="fa fa-star checked"></span>
+                            <span class="fa fa-star checked"></span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="">
+                            <span class="fa fa-star checked"></span>
+                            <span class="fa fa-star checked"></span>
+                            <span class="fa fa-star checked"></span>
+                            <span class="fa fa-star checked"></span>
+                            <span class="fa fa-star"></span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="">
+                            <span class="fa fa-star checked"></span>
+                            <span class="fa fa-star checked"></span>
+                            <span class="fa fa-star checked"></span>
+                            <span class="fa fa-star"></span>
+                            <span class="fa fa-star"></span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="">
+                            <span class="fa fa-star checked"></span>
+                            <span class="fa fa-star checked"></span>
+                            <span class="fa fa-star"></span>
+                            <span class="fa fa-star"></span>
+                            <span class="fa fa-star"></span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="">
+                            <span class="fa fa-star checked"></span>
+                            <span class="fa fa-star"></span>
+                            <span class="fa fa-star"></span>
+                            <span class="fa fa-star"></span>
+                            <span class="fa fa-star"></span>
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+
+        <div class="col-12 col-md-10">
+            <?php
+            $childCategories = App\Models\Categories\Category::find(1)->childCategories->take(6);
+            echo $childCategories;
+            ?>
+            <div class="row">
+                @foreach($childCategories as $childCategory)
+                <div class="col-2 text-center">
+                    <img class="mw-100" style="border-radius: 100%;" src="https://via.placeholder.com/640" alt="">
+                    <h5 class="pt-2">{{ $childCategory->name }}</h5>
+                </div>
+                @endforeach
             </div>
         </div>
     </div>
 </div>
 @endsection
+
+@push('style')
+<style>
+    .fa.fa-star {
+        color: #6e6e6e;
+    }
+
+    .fa.fa-star.checked {
+        color: #fccb34;
+    }
+
+    /* Custom animation */
+</style>
+@endpush
