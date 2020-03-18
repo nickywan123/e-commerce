d1b88b757f571237280ba7247e31b6d58aa6976e
wanshahruddin95 <wanshahruddin95@gmail.com>
2020-03-18 09:11:23 +0800

    Refacator customer registration.

--- resources/views/auth/register/customer.blade.php
+++ resources/views/auth/register/customer.blade.php
@@ -511,27 +511,6 @@
     // Initialize a new signaturePad instance.
     var signaturePad = new SignaturePad(canvas);
 
-    function resizeCanvas() {
-        // When zoomed out to less than 100%, for some very strange reason,
-        // some browsers report devicePixelRatio as less than 1
-        // and only part of the canvas is cleared then.
-        var ratio = Math.max(window.devicePixelRatio || 1, 1);
-
-        // This part causes the canvas to be cleared
-        canvas.width = canvas.offsetWidth * ratio;
-        canvas.height = canvas.offsetHeight * ratio;
-        canvas.getContext("2d").scale(ratio, ratio);
-
-        // This library does not listen for canvas changes, so after the canvas is automatically
-        // cleared by the browser, SignaturePad#isEmpty might still return false, even though the
-        // canvas looks empty, because the internal data of this library wasn't cleared. To make sure
-        // that the state of this library is consistent with visual state of the canvas, you
-        // have to clear it manually.
-        signaturePad.clear();
-    }
-
-
-
     // Clear signature pad.
     signatureResetButton.addEventListener("click", function(event) {
         signaturePad.clear();
@@ -546,9 +525,5 @@
             signatureInput.value = signatureUrl;
         }
     })
-
-
-    // Returns signature image as data URL (see https://mdn.io/todataurl for the list of possible parameters)
-    console.log(signaturePad.toDataURL()); // save image as PNG
 </script>
 @endpush
