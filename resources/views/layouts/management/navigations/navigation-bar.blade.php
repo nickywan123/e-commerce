83c6406cf3c32c66b2539d452a40b2f7195e60b1
wanshahruddin95 <wanshahruddin95@gmail.com>
2020-03-18 18:02:01 +0800

    Image name on dashboard.

--- resources/views/layouts/management/navigations/navigation-bar.blade.php
+++ resources/views/layouts/management/navigations/navigation-bar.blade.php
@@ -1,23 +1,23 @@
 <header class="app-header navbar navbar-bg-color">
 
     <button class="navbar-toggler sidebar-toggler d-lg-none mr-auto" type="button" data-toggle="sidebar-show">
-        <span class="navbar-toggler-icon" ></span>
+        <span class="navbar-toggler-icon"></span>
     </button>
 
-    <button class="navbar-toggler sidebar-toggler d-md-down-none"  type="button" data-toggle="sidebar-lg-show">
-        <span class="navbar-toggler-icon" ></span>
+    <button class="navbar-toggler sidebar-toggler d-md-down-none" type="button" data-toggle="sidebar-lg-show">
+        <span class="navbar-toggler-icon"></span>
     </button>
 
     <a class="navbar-brand" href="#">
-        <img class="navbar-brand-full" src="{{asset('images/Bujishu_Logo.png')}}" width="89" height="50" alt="Bujishu Logo">
-        <img class="navbar-brand-minimized" src="{{asset('images/Bujishu_Logo.png')}}" width="30" height="30" alt="Bujishu Logo">
+        <img class="navbar-brand-full" src="{{asset('images/Bujishu_logo.png')}}" width="89" height="50" alt="Bujishu Logo">
+        <img class="navbar-brand-minimized" src="{{asset('images/Bujishu_logo.png')}}" width="30" height="30" alt="Bujishu Logo">
     </a>
 
-    
+
 
     <ul class="nav navbar-nav ml-auto">
-        <h5  style="color:#ffcc00; font-size:15px; margin-top:5px;">Welcome, ABC Company</h5>
-        
+        <h5 style="color:#ffcc00; font-size:15px; margin-top:5px;">Welcome, ABC Company</h5>
+
         <li class="nav-item dropdown">
             <a class="nav-link" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">
                 <img class="img-avatar" src="{{asset('images/panel-company.png')}}" alt="{{ Auth::user()->userInfo->full_name }}">
@@ -55,6 +55,6 @@
     {{-- <button class="navbar-toggler aside-menu-toggler d-md-down-none" type="button" data-toggle="aside-menu-lg-show">
         <span class="navbar-toggler-icon"></span>
     </button> --}}
-    
-  
+
+
 </header>
