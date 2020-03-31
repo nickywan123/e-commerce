38d5c2870e5235bcf3c2b4db61f20ca747ea4f92
wanshahruddin95 <wanshahruddin95@gmail.com>
2020-03-31 14:44:35 +0800

    Added customer to user table seeders.

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -282,5 +282,61 @@ class UsersTableSeeder extends Seeder
 
         $user->assignRole('customer');
         $user->assignRole('panel');
+
+        // Customer 1
+
+        // Users table.
+        $user = new User;
+        $user->email = 'customer1@email.com';
+        $user->password = Hash::make('password');
+        $user->email_verified_at = '2020-03-28 12:12:40';
+        $user->save();
+
+        // Generating new customer account id.
+        $largestCustomerId = 0;
+        if (UserInfo::all()->count() == 0) {
+            $largestCustomerId = 1913000001;
+        } else {
+            $largestCustomerId = UserInfo::largestCustomerId() + 1;
+        }
+
+        // User_infos table.
+        $userInfo = new UserInfo;
+        $userInfo->user_id = $user->id;
+        $userInfo->account_id = $largestCustomerId;
+        $userInfo->full_name = 'Customer 1';
+        $userInfo->nric = '951119105605';
+        $userInfo->referrer_id = 0;
+        // Signature to image.
+        $userInfo->signature = '-';
+        $userInfo->save();
+
+        // User_addresses table.
+        $userAddress = new UserAddress;
+        $userAddress->account_id = $userInfo->account_id;
+        $userAddress->address_1 = 'Address 1';
+        $userAddress->address_2 = 'Address 2';
+        $userAddress->address_3 = 'Address 3';
+        $userAddress->postcode = '42000';
+        $userAddress->city = 'Klang';
+        $userAddress->state_id = 1;
+        $userAddress->is_shipping_address = 1;
+        $userAddress->is_residential_address = 1;
+        $userAddress->is_mailing_address = 1;
+        $userAddress->save();
+
+        // User_contacts table (Home).
+        $userContactHome = new UserContact;
+        $userContactHome->account_id = $userInfo->account_id;
+        $userContactHome->contact_num = '0166929202';
+        $userContactHome->is_home = 1;
+        $userContactHome->save();
+
+        // User_contacts table (Mobile).
+        $userContactMobile = new UserContact;
+        $userContactMobile->account_id = $userInfo->account_id;
+        $userContactMobile->contact_num = '0194039056';
+        $userContactMobile->is_mobile = 1;
+        $userContactMobile->save();
     }
 }
