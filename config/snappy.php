417066866125a9a03be8511c26ed05ecae5e45ec
wanshahruddin95 <wanshahruddin95@gmail.com>
2020-04-16 10:00:23 +0800

    Update snappy config

--- config/snappy.php
+++ config/snappy.php
@@ -36,8 +36,7 @@ return [
     'pdf' => [
         'enabled' => true,
         // Ubuntu
-        // 'binary'  => env('WKHTML_PDF_BINARY', '/usr/local/bin/wkhtmltopdf'),
-        'binary' => '"C:\Program Files\wkhtmltopdf\bin\wkhtmltopdf"',
+        'binary'  => env('WKHTML_PDF_BINARY', '"C:\Program Files\wkhtmltopdf\bin\wkhtmltopdf"'),
         'timeout' => false,
         'options' => [],
         'env'     => [],
