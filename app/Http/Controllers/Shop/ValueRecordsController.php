f4b1b221f962a0068db73454112ead83a249cf3c
wanshahruddin95 <wanshahruddin95@gmail.com>
2020-04-24 17:52:39 +0800

    Fix value record

--- app/Http/Controllers/Shop/ValueRecordsController.php
+++ app/Http/Controllers/Shop/ValueRecordsController.php
@@ -14,43 +14,46 @@ class ValueRecordsController extends Controller
 
     /****Return ALL ORDERS in customer dashboard  ****/
 
-    public function customerAllOrders(){
+    public function customerAllOrders()
+    {
         $user = User::find(Auth::user()->id);
         // Return purchases that user have paid
-        $purchases = $user->purchases->where('purchase_status',3003);
+        $statuses = [3001, 3002, 3003];
+        $purchases = $user->purchases->whereIn('purchase_status', $statuses);
         // $annualOrders= $user->purchases->orders->whereYear('created_at', '=', 2020)->get();
         return view('shop.customer-dashboard.value-records.index')->with('purchases', $purchases);
-       }
-    
-    
-
-     /** Show invoice of the order (PDF)**/
-
-     public function invoice($purchase_num)
-     {
-         // Get user.
-         $user = User::find(Auth::user()->id);
-         $purchase = $user->purchases->where('purchase_number', $purchase_num)->first();
-         $pdf = PDF::loadView('documents.purchase.invoice', compact('purchase'))->setPaper('A4');
-         return $pdf->stream('purchase-order.' . $purchase_num . '.pdf');
-     }
- 
- 
-     /***Show receipt of order (PDF)***/
- 
-     public function receipt($purchase_num)
-     {
- 
-         // Get user.
-         $user = User::find(Auth::user()->id);
-         $purchase = $user->purchases->where('purchase_number', $purchase_num)->first();
-         $pdf = PDF::loadView('documents.receipt.receipt', compact('purchase'))->setPaper('A4');
-         return $pdf->stream('receipt-.' . $purchase_num . '.pdf');
-     }
+    }
 
 
-     /**Return wishlist for user */
-     public function wishlist(){
-         return view('shop.customer-dashboard.value-records.wishlist');
-     }
+
+    /** Show invoice of the order (PDF)**/
+
+    public function invoice($purchase_num)
+    {
+        // Get user.
+        $user = User::find(Auth::user()->id);
+        $purchase = $user->purchases->where('purchase_number', $purchase_num)->first();
+        $pdf = PDF::loadView('documents.purchase.invoice', compact('purchase'))->setPaper('A4');
+        return $pdf->stream('purchase-order.' . $purchase_num . '.pdf');
+    }
+
+
+    /***Show receipt of order (PDF)***/
+
+    public function receipt($purchase_num)
+    {
+
+        // Get user.
+        $user = User::find(Auth::user()->id);
+        $purchase = $user->purchases->where('purchase_number', $purchase_num)->first();
+        $pdf = PDF::loadView('documents.receipt.receipt', compact('purchase'))->setPaper('A4');
+        return $pdf->stream('receipt-.' . $purchase_num . '.pdf');
+    }
+
+
+    /**Return wishlist for user */
+    public function wishlist()
+    {
+        return view('shop.customer-dashboard.value-records.wishlist');
+    }
 }
