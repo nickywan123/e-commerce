8da8edc413f9c939e854e00f0edc8b22fa40c89c
wanshahruddin95 <wanshahruddin95@gmail.com>
2020-04-29 20:18:20 +0800

    Change to react preset.

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,7 +1,7 @@
 /**
  * First we will load all of this project's JavaScript dependencies which
- * includes Vue and other libraries. It is a great starting point when
- * building robust, powerful web applications using Vue and Laravel.
+ * includes React and other helpers. It's a great starting point while
+ * building robust, powerful web applications using React + Laravel.
  */
 
 require("./bootstrap");
@@ -11,24 +11,10 @@ require("signature_pad");
 require("slick-carousel");
 require("ion-rangeslider");
 
-window.Vue = require("vue");
-
-Vue.component(
-    "example-component",
-    require("./components/ExampleComponent.vue").default
-);
-
-Vue.component(
-    "product-table",
-    require("./components/Administrator/Products/ProductTable.vue").default
-);
-
 /**
- * We will create a fresh Vue application instance and attach it to
+ * Next, we will create a fresh React component instance and attach it to
  * the page. Then, you may begin adding components to this application
  * or customize the JavaScript scaffolding to fit your unique needs.
  */
 
-const app = new Vue({
-    el: "#app"
-});
+require('./components/Example');
