e4d51a233790d23bee5ec208ebf32f5610e3b61b
wanshahruddin95 <wanshahruddin95@gmail.com>
2020-05-19 09:56:02 +0800

    Changed font to Lato
    Reduced 'by {vendor}' on product page font size.

--- resources/views/layouts/shop/main.blade.php
+++ resources/views/layouts/shop/main.blade.php
@@ -16,7 +16,7 @@
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Lato" rel="stylesheet">
     <link href='https://fonts.googleapis.com/css?family=Tangerine' rel='stylesheet'>
 
 
@@ -36,7 +36,6 @@
 
     <!-- Navigation bar -->
     @include('layouts.shop.navigation.navigation')
-    {{-- @livewire('counter') --}}
 
     <!-- Side bar -->
     @include('layouts.shop.navigation.sidebar')
